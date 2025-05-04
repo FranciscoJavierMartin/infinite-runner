76a6c455eb7324a4365d010e36d9ab8a86edcf98
Francisco <github.backstage538@passmail.net>
2025-05-04 21:21:10 +0200

    Get canvas

---
+++ README.md
@@ -0,0 +1,3 @@
+# Infinite Runner
+
+A simple infinite runner game built with HTML, CSS, and JavaScript. Play [here](https://franciscojaviermartin.github.io/infinite-runner/).
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
 import './style.css';
+
+class Game {
+  private canvas: HTMLCanvasElement;
+  private ctx: CanvasRenderingContext2D;
+
+  constructor() {
+    this.canvas = document.getElementById('game-canvas') as HTMLCanvasElement;
+    this.ctx = this.canvas.getContext('2d')!;
+
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
+  }
+
+  public render(): void {
+    this.ctx.fillStyle = '#0a0c21';
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+}
+
+window.onload = () => {
+  const game = new Game();
+  game.render();
+};
